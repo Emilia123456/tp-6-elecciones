0758b99e5cdf5d39b45c6dfcd8e88235e0ca5276
MATEO SVERDLOFF <47572192@ort>
2023-06-29 15:54:56 -0300

    puse las clases (falta lista partidos/candidatos)

--- Models/BD.cs
+++ Models/BD.cs
@@ -1,9 +1,54 @@
 using System.Data.SqlClient;
 using Dapper;
+public class BD{
 
-/*
-private static string _connectionString = @”Server=localhost”;
-DataBase=Elecciones2023;Trusted_Connection=True;”;
-*/
+    private static List<Partido> _ListadoPartidos = new List<Partido>{};
+    private static List<Candidato> _ListadoCandidatos = new List<Candidato>{};
 
+    private static string _connectionString = @"Server=localhost;
+    DataBase=Elecciones2023;Trusted_Connection=True;";
 
+    public void CargarPartido(){
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Pantentes";
+            _ListadoPartidos=db.Query<Partido>(sql).ToList();
+        }
+    }
+
+    public void CargarCandidato(){
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Candidato";
+            _ListadoCandidatos=db.Query<Candidato>(sql).ToList();
+        }
+    }
+
+    public void AgregarCandidato(Candidato can){
+        string SQL = "INSERT INTO Candidatos(IdPartido, Nombre, Apellido, Foto, Pustulcaion, FechaNacimiento) Values (@pIdPartido, @pNombre, @pApellido,  @pFoto, @pPustulcaion, @pFechaNacimiento,)";
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            db.Execute(SQL, new{pIdPartido = can.IdPartido, pNombre=can.Nombre, pApellido=can.Apellido, pFoto=can.Foto, pPostulacion=can.Postulacion, pFechaNacimiento=can.FechaNacimiento});
+        }
+    }
+
+    public void ElmiminarCandidato(int IdCandidato){
+        string SQL = "DELETE FROM Candidatos WHERE IdCandidato = @id";
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+        db.Execute(SQL, new{id = IdCandidato});
+        }
+    }
+
+    public static Partido VerInfoPartido(int idPartido){
+        for(int i=0; i<_ListadoPartidos.Count(); idPartido++){
+            if(_ListadoPartidos[i].IdPartido == idPartido) { return _ListadoPartidos[i];}
+        }
+        return null;
+    }
+
+    public static Candidato VerInfoCandidato(int idCandidato){
+        for(int i=0; i<_ListadoCandidatos.Count(); i++){
+            if(_ListadoCandidatos[i].IdCandidato == idCandidato) { return _ListadoCandidatos[i];}
+        }
+        return null;
+    }
+
+    
+}
--- Models/Candidato.cs
+++ Models/Candidato.cs
@@ -1,12 +1,13 @@
 public class Candidato{
-    public static int IdCandidato {get; set;}
-    public static int IdPartido {get; set;}
-    public static string Apellido {get; set;}
-    public static DateTime FechaNacimiento {get; set;}
-    public static string Foto {get; set;}
-    public static string Postulacion {get; set;}
-    public static string Nombre {get; set;}
+    public int IdCandidato {get; set;}
+    public int IdPartido {get; set;}
+    public string Apellido {get; set;}
+    public DateTime FechaNacimiento {get; set;}
+    public string Foto {get; set;}
+    public string Postulacion {get; set;}
+    public string Nombre {get; set;}
 
     public Candidato(int idcandidato, int idpartido, string nombre, string apellido, DateTime fechanacimiento, string foto, string postulacion){
+        
     }
 }
