b29149e0c5cd746ac6676b72417737521cfc3653
EMILIA INÉS ARMENO <47697893@ort>
2023-06-29 13:24:19 -0300

    creamos el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tp_6_elecciones.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
